b38ac190e16757206b7e30d783fd6ce04aae086d
SGrodnik <s.grodnik@genpro.ru>
2022-04-02 21:42:01 +0300

    Add a new field

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -14,5 +14,7 @@ namespace RazorPagesMovie.Models
         public string Genre { get; set; } = string.Empty;
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [Display(Name = "Рейтинг")]
+        public string Rating { get; set; } = string.Empty;
     }
 }
